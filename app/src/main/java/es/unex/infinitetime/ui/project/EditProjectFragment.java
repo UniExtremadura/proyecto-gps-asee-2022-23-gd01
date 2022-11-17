b2ea29636409b21952b56c4ebd0e2dc354cfb29f
GD01Marron <javier.berrocal.olmeda@gmail.com>
2022-11-17 10:38:43 +0100

    PGD01-225 IT7 - I - UC16 - Implement Components #done #time 6h 37m En el fragmento de editar proyecto precargar los datos del proyecto.Una vez que los datos hayan sido modificados del proyecto y se pulse el botón confirmar actualizar los datos del usuario con los nuevos datos.

--- app/src/main/java/es/unex/infinitetime/ui/project/EditProjectFragment.java
+++ app/src/main/java/es/unex/infinitetime/ui/project/EditProjectFragment.java
@@ -7,22 +7,40 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import es.unex.infinitetime.AppExecutors;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import es.unex.infinitetime.persistence.InfiniteDatabase;
+import es.unex.infinitetime.persistence.Project;
+import es.unex.infinitetime.persistence.Task;
+import es.unex.infinitetime.persistence.TaskState;
+import es.unex.infinitetime.persistence.User;
+
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import es.unex.infinitetime.databinding.FragmentEditProjectBinding;
 import es.unex.infinitetime.R;
 
-/**
- */
+
 public class EditProjectFragment extends Fragment {
 
+    private static final String ARG_PARAM1 = "project_id";
+
     private long projectId;
-    private static String ARG_PARAM1 = "project_id";
+
     private FragmentEditProjectBinding binding;
+
+
+
 
     FloatingActionButton confirmEditCheck;
     FloatingActionButton cancelEditCheck;
@@ -55,9 +73,62 @@ public class EditProjectFragment extends Fragment {
         confirmEditCheck = binding.checkEditBtn;
         cancelEditCheck = binding.cancelEditBtn;
 
+
+        // Borrar el siguiente código al integrar el caso de uso
+
+        User user = new User();
+        user.setId(1332);
+        user.setUsername("username1234");
+        user.setEmail("hola@gmail.com");
+        user.setPassword("1234");
+
+        Project insertProject = new Project();
+        insertProject.setId(projectId);
+        insertProject.setName("Project 1");
+        insertProject.setDescription("Description 1");
+        insertProject.setUserId(user.getId());
+
+
+        AppExecutors.getInstance().diskIO().execute(() -> {
+            if(InfiniteDatabase.getDatabase(getContext()).userDAO().getUser(user.getUsername()) == null){
+                InfiniteDatabase.getDatabase(getContext()).userDAO().insert(user);
+            }
+            if(InfiniteDatabase.getDatabase(getContext()).projectDAO().getProject(projectId) == null){
+                InfiniteDatabase.getDatabase(getContext()).projectDAO().insert(insertProject);
+            }
+        });
+
+
+        // Borrar el código anterior cuando se realice la integración
+
+        AppExecutors.getInstance().diskIO().execute(() -> {
+            Project project = InfiniteDatabase.getDatabase(getContext()).projectDAO().getProject(projectId);
+
+
+            AppExecutors.getInstance().mainThread().execute(() -> {
+                binding.ProjectNameEdit.setText(project.getName());
+                binding.ProjectDescriptionEdit.setText(project.getDescription());
+                Log.d("Depurando", "Usuario y proyecto insertado");
+            });
+        });
         confirmEditCheck.setOnClickListener(v -> {
             // Implementar lógica de edición de proyecto
+            String projectName= binding.ProjectNameEdit.getText().toString();
+            String projectDescription=binding.ProjectDescriptionEdit.getText().toString();
+            Project updateProject=new Project();
+            updateProject.setId(projectId);
+            updateProject.setUserId(user.getId());
+            updateProject.setName(projectName);
+            updateProject.setDescription(projectDescription);
+            AppExecutors.getInstance().diskIO().execute(() -> {
+
+                if(InfiniteDatabase.getDatabase(getContext()).projectDAO().getProject(projectId) != null){
+                    InfiniteDatabase.getDatabase(getContext()).projectDAO().update(updateProject);
+                    Snackbar.make(view, "Proyecto actualizado", Snackbar.LENGTH_LONG).show();
+                }
+            });
             Navigation.findNavController(v).navigate(R.id.action_editProjectFragment_to_projects);
+
         });
 
         cancelEditCheck.setOnClickListener(v -> {
