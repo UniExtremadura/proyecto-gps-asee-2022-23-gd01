3f079f88cc1ed71256ded172d576d0be0d0ef1ca
Alejandro <afernandqff@alumnos.unex.es>
2022-12-14 16:33:17 +0100

    Eliminar el import es.unex.infinitetime.datosEjemplo.ExampleData al no ser necesario

--- app/src/main/java/es/unex/infinitetime/ui/favorite/FavoriteFragment.java
+++ app/src/main/java/es/unex/infinitetime/ui/favorite/FavoriteFragment.java
@@ -20,7 +20,6 @@ import java.util.List;
 import es.unex.infinitetime.AppExecutors;
 import es.unex.infinitetime.R;
 import es.unex.infinitetime.databinding.FragmentFavoriteBinding;
-import es.unex.infinitetime.datosEjemplo.ExampleData;
 import es.unex.infinitetime.persistence.InfiniteDatabase;
 import es.unex.infinitetime.persistence.Project;
 import es.unex.infinitetime.persistence.Task;
